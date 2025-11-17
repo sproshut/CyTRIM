6ec14034bf451048c9c06ea00b41e280f1ab9f73
Proshutinskiy Sergey <igrbut34i5bygi@gmail.com>
2025-11-17 21:44:48 +0100

    Extended banchmark with different nion test, added avg times plotting

---
+++ plot_times.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# plt.rcParams.update(
+#     {
+#         "font.size": 6,  # Default text size
+#         "axes.titlesize": 14,  # Title size
+#         "axes.labelsize": 10,  # X/Y label size
+#         "xtick.labelsize": 8,  # Tick label sizes
+#         "ytick.labelsize": 8,
+#         "legend.fontsize": 8,
+#     }
+# )
+
+avg_times = np.fromfile("perf/avg_times.np").reshape(4, -1)
+nions = np.array([50, 100, 500, 1000, 10000, 100000, 500000, int(1e6)])
+funcs = ["orig", "numba", "bulk", "numba_bulk"]
+
+plt.style.use("dark_background")
+plt.title("Overview of simulation times")
+for fname, times in zip(funcs, avg_times):
+    valid_times = times[times > 1e-4]
+    plt.plot(nions[: valid_times.size], valid_times, marker="o", label=fname)
+    for nion, time in zip(nions[: valid_times.size], valid_times):
+        text = f"{float(time):.2f}"
+        plt.annotate(
+            text,
+            (nion, time),
+            textcoords="offset pixels",
+            xytext=(-(len(text) * 10), -5),
+        )
+plt.legend()
+plt.tight_layout()
+plt.xscale("log")
+plt.yscale("log")
+plt.xlabel("Number ions simulated")
+plt.ylabel("Time (s)")
+# plt.savefig("media/sim_perf.png", dpi=300, transparent=True)
+plt.show()
--- pytrim/main.py
+++ pytrim/main.py
@@ -9,53 +9,56 @@ from tqdm import tqdm
 
 import pytrim
 
-nion = 1000
-iter_count = 5
+test_nions = [100, 1000, 10000, 100000]
+iter_count = 3
+test_funcs = [
+    # {
+    #     "func": pytrim.simulate,
+    #     "label": "orig",
+    #     "compilable": False,
+    #     # "iter_override": 1,
+    #     # "min_nion_test": True,
+    # },
+    {"func": pytrim_numba.simulate, "label": "numba", "compilable": True},
+    {"func": pytrim_bulk.simulate, "label": "bulk", "compilable": False},
+    {"func": pytrim_numba_bulk.simulate, "label": "numba_bulk", "compilable": True},
+]
 
 if __name__ == "__main__":
-    times = []
-    data = []
-    test_funcs = [
-        # {
-        #     "func": pytrim.simulate,
-        #     "label": "orig",
-        #     "compilable": False,
-        #     "iter_override": 1,
-        # },
-        {"func": pytrim_numba.simulate, "label": "numba", "compilable": True},
-        {"func": pytrim_bulk.simulate, "label": "bulk", "compilable": False},
-        {"func": pytrim_numba_bulk.simulate, "label": "numba_bulk", "compilable": True},
-    ]
-    plt.style.use("dark_background")
-    plt.title("Comparison of different simulations")
-    for i in tqdm(range(iter_count), desc="Current iteration"):
-        times.append([])
-        for func_def in tqdm(test_funcs, desc="Progress", leave=False):
-            if "iter_override" in func_def.keys():
-                it_ovr = func_def["iter_override"]
-                if i + 1 > it_ovr:
-                    times[i].append(it_ovr - 1)  # TODO Should preserve mean???
+    avg_times = np.zeros((len(test_funcs), len(test_nions)))
+    z_pos = []
+    for i in tqdm(range(iter_count), desc="Iterations"):
+        for n_i, nion in enumerate(nion_pbar := tqdm(test_nions, leave=False)):
+            nion_pbar.set_description_str(f"{nion} ions")
+            for f_i, func_def in enumerate(func_pbar := tqdm(test_funcs, leave=False)):
+                func_pbar.set_description_str(f"in {func_def['label']}")
+                if func_def.get("min_nion_test") and n_i != 0:
                     continue
-            if func_def["compilable"]:
-                func_def["func"](nion)  # Dry-run to compile first
-                # Prevent re-compilation on next iteration
-                func_def["compilable"] = False
-            start = time.time()
-            pos, dir, e, is_inside, *other = func_def["func"](nion)
-            times[i].append(time.time() - start)
-            if i == iter_count - 1:
-                if isinstance(pos, list):  # Assuming everything else is list as well
-                    pos = np.array(pos)
-                    dir = np.array(dir)
-                    e = np.array(e)
-                    is_inside = np.array(is_inside)
-                data.append(pos[is_inside, 2])
+                if "iter_override" in func_def.keys():
+                    it_ovr = func_def["iter_override"]
+                    if i + 1 > it_ovr:
+                        continue
+                if func_def["compilable"]:
+                    func_def["func"](nion)  # Dry-run to compile first
+                    # Prevent re-compilation on next iteration / ion count
+                    func_def["compilable"] = False
 
-    avg_times = [0.0 for _ in range(len(test_funcs))]
-    for j in range(len(test_funcs)):
-        avg_times[j] = sum(times[i][j] for i in range(iter_count)) / iter_count
-    print(dict(zip([f["label"] for f in test_funcs], avg_times)))
-    plt.boxplot(data)
+                start = time.time()
+                pos, dir, e, is_inside, *other = func_def["func"](nion)
+                avg_times[f_i, n_i] += time.time() - start
+                if n_i == 0 and i == iter_count - 1:
+                    if isinstance(pos, list):
+                        # Assuming everything else is a list as well
+                        pos = np.array(pos)
+                        is_inside = np.array(is_inside)
+                    z_pos.append(pos[is_inside, 2])
+            (avg_times / (i + 1)).tofile("avg_times_3.np")
+
+    avg_times /= iter_count
+    print(avg_times)
+    plt.style.use("dark_background")
+    plt.title("Z depth across different simulations")
+    plt.boxplot(z_pos)
     plt.xticks(
         [i + 1 for i in range(len(test_funcs))], [f["label"] for f in test_funcs]
     )
